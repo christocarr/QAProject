da7050165266d8498cca2dae05fde75e4c3a8e00
christocarr <christocarr@outlook.com>
2022-08-20 14:17:29 +0100

    chore: add crud repositories

--- src/main/java/com/christocarr/qaproject/repository/CarRepository.java
+++ src/main/java/com/christocarr/qaproject/repository/CarRepository.java
@@ -1,10 +1,10 @@
 package com.christocarr.qaproject.repository;
 
-import com.christocarr.qaproject.model.Customer;
+import com.christocarr.qaproject.model.Car;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CustomerRepository extends CrudRepository<Customer, Integer> {
+public interface CarRepository extends CrudRepository<Car, Integer> {
 
 }
--- src/main/java/com/christocarr/qaproject/repository/OnHireRepository.java
+++ src/main/java/com/christocarr/qaproject/repository/OnHireRepository.java
@@ -1,10 +1,10 @@
 package com.christocarr.qaproject.repository;
 
-import com.christocarr.qaproject.model.Customer;
+import com.christocarr.qaproject.model.OnHire;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CustomerRepository extends CrudRepository<Customer, Integer> {
+public interface OnHireRepository extends CrudRepository<OnHire, Integer> {
 
 }
