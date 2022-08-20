4d84904cdb259e51d7ec8daaff5eb645e97d846b
christocarr <christocarr@outlook.com>
2022-08-20 11:26:32 +0100

    chore: test and code customer model

---
+++ src/main/java/com/christocarr/qaproject/model/Customer.java
@@ -0,0 +1,36 @@
+package com.christocarr.qaproject.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.lang.Nullable;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+public class Customer {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column
+  private int customerId;
+  @Column
+  private String firstName;
+  @Column
+  private String lastName;
+  @Column
+  private String addressOne;
+  @Column
+  @Nullable
+  private String addressTwo;
+  @Column
+  private String city;
+  @Column
+  private String postcode;
+  @Column
+  private String driversLicense;
+}
